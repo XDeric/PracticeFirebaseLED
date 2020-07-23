787da37392f356a01d2775e1fb5f36c287ed61d4
eeeric3995@yahoo.com <ihxx2s7tbu0>
2020-07-23 12:07:33 -0400

    starting new LED proj

---
+++ FirebaseLED/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseLED
+//
+//  Created by EricM on 7/23/20.
+//  Copyright © 2020 EricM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
