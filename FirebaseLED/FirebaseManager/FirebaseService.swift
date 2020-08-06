96691d4daa9a632fd3e4906e17787a7ab6f1f368
eeeric3995@yahoo.com <ihxx2s7tbu0>
2020-08-06 01:29:28 -0400

    creating model for button status

---
+++ FirebaseLED/FirebaseManager/FirebaseService.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseService.swift
+//  FirebaseLED
+//
+//  Created by EricM on 8/6/20.
+//  Copyright © 2020 EricM. All rights reserved.
+//
+
+import Foundation
---
+++ FirebaseLED/Model/Status.swift
@@ -0,0 +1,35 @@
+//
+//  Status.swift
+//  FirebaseLED
+//
+//  Created by EricM on 8/6/20.
+//  Copyright © 2020 EricM. All rights reserved.
+//
+
+import Foundation
+
+struct Status: FirebaseConvertible {
+    let id: UUID
+    let status: String
+    let statusNum: Int
+}
+
+protocol FirebaseConvertible: Codable {
+    func fieldsDict() -> [String: Any]
+    init?(fromFirebaseDict dict: [String: Any])
+}
+
+extension FirebaseConvertible {
+    init?(fromFirebaseDict dict: [String: Any]) {
+        do {
+            let data = try JSONSerialization.data(withJSONObject: dict)
+            let object = try JSONDecoder().decode(Self.self, from: data)
+            self = object
+        } catch {
+            return nil
+        }
+    }
+    func fieldsDict() -> [String: Any] {
+        return try! JSONSerialization.jsonObject(with: JSONEncoder().encode(self)) as! [String: Any]
+    }
+}
