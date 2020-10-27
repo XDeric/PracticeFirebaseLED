d0e98893c0ab455b4adb8d30ade5c4f9144c413c
XDEric <eeeric3995@yahoo.com>
2020-10-27 19:24:10 -0400

    adding test to see if firebase responds

---
+++ FirebaseLED/testinfFire.swift
@@ -0,0 +1,9 @@
+//
+//  testinfFire.swift
+//  FirebaseLED
+//
+//  Created by EricM on 10/27/20.
+//  Copyright © 2020 EricM. All rights reserved.
+//
+
+import Foundation
--- FirebaseLEDTests/FirebaseLEDTests.swift
+++ FirebaseLEDTests/FirebaseLEDTests.swift
@@ -10,25 +10,31 @@ import XCTest
 @testable import FirebaseLED
 
 class FirebaseLEDTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func testFirebaseAndUI(){
+        let dService = DatabaseService.manager.test
+        print(dService)
+        //XCTAssertEqual(<#T##expression1: Equatable##Equatable#>, <#T##expression2: Equatable##Equatable#>)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    override func setUp() {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDown() {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
